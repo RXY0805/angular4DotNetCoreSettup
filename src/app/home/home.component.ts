f020b026d13fb6107a8dc426f5cc5403f50ba3ce
Wade Ren <wr@3dsafety.com.au>
2017-10-26 13:14:53 +1100

    Change to h1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import {Component}  from '@angular/core';
 
 @Component ({
     selector: 'app-home',
-    template: '<p>you are logged in</p>',
+    template: '<h1>you are logged in</h1>',
     styles: []
     // templateUrl: './home.component.html',
     // styleUrls: []
